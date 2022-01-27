ad9d555b42e7f79f63f6372ada6b5371373eb450
James Lin <wonyy1988@outlook.com>
2022-01-27 14:31:20 -0800

    update page 14, 60

--- src/components/magazines/issue-9/Page5960.js
+++ src/components/magazines/issue-9/Page5960.js
@@ -27,37 +27,37 @@ const Page5960 = () => {
           height={278}
         />
       </div>
-      <div className={styles.text1}>Elyoya</div>
+      <div className={styles.text1}>BUGHA</div>
       <div className={styles.text2}>E-SPORTS RISING STAR OF THE MONTH</div>
       <div className={styles.text3}>
-        Javier "Elyoya" Prades Batalla is a League of Legends esports player and currently the jungler for MAD Lions. He is a Spanish player and known best for his continued grit and determination, where even in the face of some of the most adverse gameplay, he pushes himself through mental and physical barriers to bring home some of the most significant team wins. This was more than evident this year where his results on the international stage speak for themselves. 
+        16 year old Kyle “Bugha” Giersdorf is considered a Fortnite prodigy, taking the competitive gaming world by storm with racked up wins and streaked kills. It’s not without sacrafice, grit and determination, as Kyle often expresses himself during interviews. But the thrill that the hard work is in fact paying off is asymmetrical enough to outweigh any short term discontent. 
       </div>
       <div className={styles.text4}>metaverse ready score: 6</div>
-      <div className={styles.text5}>ZAI</div>
+      <div className={styles.text5}>METHODZ</div>
       <div className={styles.text6}>E-SPORTS PLAYER OF THE MONTH</div>
-      <div className={styles.text7}>metaverse ready score: 7</div>
+      <div className={styles.text7}>metaverse ready score: 9</div>
       <div className={styles.text8}>
-        At 24 Zai Ludwig Wåhlberg is one of the sports most battle tested and fierce in-game competitors. His winnings overall have amassed him more than $3m in prize money as he constantly sweeps the floor and does Sweden proud. One of this Dota 2 players most significant wins was that in October this year, where he placed 3rd at The International 2021— one of his best tournaments out of the 106 previous comps he’s competed in. 
+        Anthony Zinni, AKA METHODZ, is a Call of Duty Otaku. Hailing from Boston, he began his career on COD 4: Modern Warfare for the PlayStation 3 at 11 years old. He didn’t begin competing in esports tournaments until COD: Modern Warfare 2, and since then continues to impress. He’s been on top teams such as SoaR Gaming, FaZe Clan, Team Curse, Team Kaliber, Strictly Business, OpTic Gaming, UyU, and the Toronto Ultra.
       </div>
-      <div className={styles.text9}>fazes of the moon and more</div>
+      <div className={styles.text9}>NVIDIA RTX GeForce 3050</div>
       <div className={styles.text10}>
-        FaZe Clan has officially announced MoonPay as their infrastructure provider and partner for all upcoming NFT drops, integrations and other cryptocurrency related activities. 
+        Fully sold out. Was it that surprising? 
         <br /><br />
-        But it doesn’t just stop there, FaZe is taking this move into the web3 space with full force, where alongside MoonPay they will be instantiating a new project arm, FaZe Forever. 
-        <br /><br />
-        It’s the first big step into the Esports world for MoonPay and a strong signal from FaZe as they bet deeper on the solid foundations that web3 provides for competitive gaming. 
-        <br /><br />
-        The new FaZe Forever project is still operating largely in stealth mode, however, it was revealed that a large part of the business will focus on curating, collecting and collaborating with other native NFT projects. 
+        Graphics engineering continues to exponentially advance across both the manufacturing and showroom floors. And with that, at the release of the GeForce RTX 3050 8GB, Nvidia’s ray-tracing entry-level graphics card, it’s really no shock that there was an immediate “out of stock” announcement broadcast from all sellers. 
+        <br /><br /><br />
+        The interesting point of consideration here though is more on whether or not big hardware manufactureres like NVIDIA will ever drive forth the open source hardware revolution— it’s likely they won’t— however, with the continued onset and adoption of web3, complimentary OS hardware innovations are on the rise, and soon will become an unstoppable force. Pivot or die. 
       </div>
-      <div className={styles.text11}>Education provisions for esports</div>
+      <div className={styles.text11}>EXCHANGES AND CRYPTO SPONSORS</div>
       <div className={styles.text12}>
-        Confetti Institute of Creative Technologies is not the first to transform their educational platform into catering to the Esports community and establishing graduation certificates for Esports players, however, it’s approach is rather more left afield than other schools. The Institute is focused on bringing the commercial nature of the industry directly into the classroom, where they are building a multi-event esports venue for live matches right on the campus. 
-      </div>
-      <div className={styles.text13}>MOBILE ESPORTS KEEPS BOOMING</div>
-      <div className={styles.text14}>
-        Southeast Asia has always been alot more in check with the mobile esports sector than many western countries, where it still seems to go unnoticed at large. 
+        After the first rise in the bull run from 2021, it’s no surprise that a flood of exchanges have started throwing sponsorship deals at some of the hottest esports team, as of now. Bitget and Bithumb are just two of the like. 
         <br /><br />
-        An interesting insight shared by Tier One Entertainment reveals that the eastern markets much prefer spending smaller amounts during game play through microtransactions, even if the cost is greater overall. Hence the lean towards mobile gaming. Will adoption of this new interface for play keep us twitching out of the metaverse? 
+        Sure, it’s good for more crypto exposure— but is a centralised exchange really the best representation of the vitality of the web3 community & where the most value opportunity is for gaming collaborations? No, definitely not. 
+      </div>
+      <div className={styles.text13}>CSMG AND ESPORTSU</div>
+      <div className={styles.text14}>
+        24/7 Esports Twitch streaming is coming to a computer screen near you. ESPORTU is being set up as a continuous always on live gaming stream. 
+        <br /><br />
+        There is a bit of a cool twist to it, the streams will be dedicated to showcasing college esports projects and events— not the typical mainstream mass produced content that usually dominates the visuals. Time to find some raw upcoming talent. 
       </div>
 
       <div className={styles.esportsText1}>E-SPORTS BUZZZZ</div>
