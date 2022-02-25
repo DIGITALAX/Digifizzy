1a6c126a779e28dedf4a91d12fb2a957f2ecdb77
James Lin <wonyy1988@outlook.com>
2022-02-25 04:53:41 -0800

    feat: update 89, 90, 91, 92 pages on issue 10

--- src/components/magazines/issue-10/Page8990.js
+++ src/components/magazines/issue-10/Page8990.js
@@ -8,7 +8,7 @@ const Page8990 = () => {
     <div className={styles.wrapper}>
       <div className={styles.imageBackground}>
         <Image
-          src='/magazine/9/images/8990_background.jpg'
+          src={`${DIGIFIZZY_S3}/10/images/8990_background.jpg`}
           width={960}
           height={1497}
         />
@@ -28,44 +28,81 @@ const Page8990 = () => {
       <div className={styles.text9}>
         WHAT ARE YOU WEARING?
       </div>
+      <div className={styles.rect1}></div>
       <div className={styles.text10}>
         FASHION FOR THE METAVERSE
       </div>
 
-      <div className={styles.text1}>
-        False Hope & Fake Positives
+      <div className={styles.circle1}></div>
+      <div className={styles.circle2}></div>
+      <div className={styles.circle3}></div>
+
+      <a
+        className={styles.link1}
+        href='https://www.realmrunway.xyz/'
+        target='_blank'
+      >
+        <div className={styles.text1}>
+          Realm Runway
+        </div>
+        <div className={styles.text3}>
+          Realm Runway
+        </div>
+        
+      </a>
+      <a
+        className={styles.link2}
+        href='https://www.realmrunway.xyz/'
+        target='_blank'
+      >
+        <div className={styles.text2}>
+          Realm Runway
+        </div>
+        <div className={styles.text4}>
+          Realm Runway
+        </div>
+      </a>
+
+      <div className={styles.text11}>
+        This month the first realm runway event was hosted by the gdn + gms during nyfw and was a huge success!
       </div>
-      <div className={styles.text2}>
-        Assessing priors effectively and being able to most suitably judge the best reality of whatever is given to us at any moment in time, is becoming exceedingly difficult as corporates increase hype marketing and push mass produced knock offs into the market. 
-        <br /><br />
-        How can we really know what’s real from what’s fake? What’s authentic from what’s a somatose simpdom? 
-        <br /><br />
-        Is it getting harder or easier with the new web3 tooling available to us? Afterall, provenance is a compelling promise but has it gained enough cultural appeal to actually be used. 
-        <br /><br />
-        It seems that for now, the spillover glamour of shadow status begged and borrowed from the old famous still runs the day. 
-        <br /><br />
-        Indications that something is something, when it’s really not, are dangerous, especially if its an indication for anything significant and potentially life changing & course charting.
-        <br /><br />
-        It’s all too easy to interpret random noise as important results and neglect the obviously skewed incentives for outcomes and keeping up with apparent status at all costs regardless of any truth contained within.
-        <br /><br />
-        This is all too true with physical space. Especially galleries and places that you’d expect creativity to be welcomed warmly and ignited. 
-      </div>
-      <div className={styles.text3}>
-        Yet, why is it so, that whenever walking into these places that it feels like the life’s been sucked out of you. It’s almost uncanny, but not quite as interesting. 
-        <br /><br />
-        False positives are everywhere, and great posturing. 
-        <br /><br />
-        Mixing web3 with adverse selection, most of these galleries have insider order books, with set limit orders that are outside of your actionable liquidity. It’s even worse when you think of the impact of false positives and adverse selection on information that leads to social group cliques. 
-        <br /><br />
-        Unsurprisingly, there’s a not so secret superpower contained within decentralised ledgers. It is the immutability of transparent records, and how systems of social coordination built on top of them provide a much more fruitful and prosperous alternative.
-        <br /><br />
-        yay, web3. Did you just give us a statistical antidote to the replication and capital circulation crisis in one fell swoop? 
-        <br /><br />
-        Having visited countless galleries in this city in a short time, I certainly think so. 
-      </div>
-      <div className={styles.text4}>
-        False Hope & Fake Positives
-      </div>      
+
+      <video muted loop autoPlay className={[styles.video1, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video1.mp4`}
+          type='video/mp4'
+        />
+      </video>
+      <video muted loop autoPlay className={[styles.video2, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video2.mp4`}
+          type='video/mp4'
+        />
+      </video>
+      <video muted loop autoPlay className={[styles.video3, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video3.mp4`}
+          type='video/mp4'
+        />
+      </video>
+      <video muted loop autoPlay className={[styles.video4, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video4.mp4`}
+          type='video/mp4'
+        />
+      </video>
+      <video muted loop autoPlay className={[styles.video5, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video5.mp4`}
+          type='video/mp4'
+        />
+      </video>
+      <video muted loop autoPlay className={[styles.video6, styles.gradient].join(' ')}>
+        <source
+          src={`${DIGIFIZZY_S3}/10/videos/8990_video6.mp4`}
+          type='video/mp4'
+        />
+      </video>
     </div>
   )
 }
--- src/components/magazines/issue-10/Page9192.js
+++ src/components/magazines/issue-10/Page9192.js
@@ -5,7 +5,113 @@ import styles from './page9192.module.scss'
 
 const Page9192 = () => (
   <div className={styles.wrapper}>
-  
+    <div className={styles.rect1}></div>
+    <div className={styles.image1}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image1.png`}
+        width={486}
+        height={1497}
+      />
+    </div>
+    <div className={styles.image2}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image2.png`}
+        width={89}
+        height={1497}
+      />
+    </div>
+    <div className={[styles.image3, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image3.jpg`}
+        width={461}
+        height={504}
+      />
+    </div>
+    <div className={[styles.image4, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image4.jpg`}
+        width={504}
+        height={451}
+      />
+    </div>
+    <div className={[styles.image5, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image5.jpg`}
+        width={277}
+        height={369}
+      />
+    </div>
+    <div className={[styles.image6, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image6.jpg`}
+        width={400}
+        height={300}
+      />
+    </div>
+    <div className={[styles.image7, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image7.jpg`}
+        width={338}
+        height={619}
+      />
+    </div>
+    <div className={[styles.image9, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image9.jpg`}
+        width={556}
+        height={417}
+      />
+    </div>
+    <div className={[styles.image8, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image8.jpg`}
+        width={339}
+        height={309}
+      />
+    </div>
+    <div className={[styles.image10, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image10.jpg`}
+        width={440}
+        height={330}
+      />
+    </div>
+    <div className={[styles.image11, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image11.jpg`}
+        width={339}
+        height={433}
+      />
+    </div>
+    <div className={[styles.image12, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image12.jpg`}
+        width={428}
+        height={321}
+      />
+    </div>
+    <div className={[styles.image13, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image13.jpg`}
+        width={227}
+        height={302}
+      />
+    </div>
+    <div className={[styles.image14, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image14.jpg`}
+        width={343}
+        height={257}
+      />
+    </div>
+    <div className={[styles.image15, styles.gradient].join(' ')}>
+      <Image
+        src={`${DIGIFIZZY_S3}/10/images/9192_image15.jpg`}
+        width={471}
+        height={353}
+      />
+    </div>
+
   </div>
 )
 
