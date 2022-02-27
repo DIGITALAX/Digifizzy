6267d06485d48b04d7c89515ac9fab8ad833803b
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-02-27 11:59:42 -0500

    add

--- src/components/magazines/issue-10/Page2526.js
+++ src/components/magazines/issue-10/Page2526.js
@@ -38,21 +38,21 @@ const Page2526 = () => {
       <div className={styles.text1}>
         Creators are moving en masse to web3 as the inexorable march towards a full-on changing of the guards takes place.
         <br /><br />
-        After all, web3 has opened the doors and provided the tools for every single creator out there–animal, vegetable, or mineral–to create a fair ecosystem that creates advocates out of creators, owners (and therefore long-term commitment) out of fans, and hopefully sustainable wealth for every stakeholder involved with the project.
+        After all, web3 has opened the doors and provided the tools for every single creator out there–animal, vegetable, or mineral–to establish a fair ecosystem that  advocates for creators and installs direct owners (and therefore long-term commitment), and evermore so, as the market continues to mature, sustainable wealth for every stakeholder involved with the project.
         <br /><br />
-        Each stakeholder is essentially a part-owner of the network, setting the flywheel alight, and empowering primary market producers with the grit, grind, and gumption to carve out their own personal realm in the space and find their tribe, so to speak.
+        Each stakeholder is essentially a part-owner of the network, setting the flywheel alight, and empowering primary market producers with the grit, grind, and gumption to carve out their own personal realm within the metaverse and find their tribe, so to speak.
         <br /><br />
-        And when these network effects build up, as more individuals come into the space, the more others are inclined to join the web3 revolution and seize the means of production for themselves, contribute and give value to communities, and promote an inclusive, dynamic, and open culture for creators to thrive.
+        And when these network effects build up, as more individuals move into the space, the more others are inclined to join the revolution and seize the means of production for themselves, contribute and give value to communities, and promote an inclusive, dynamic, and open culture for creators to thrive.
         <br /><br />
         But let this author first disabuse the notion that anyone coming into the web3 or NFT space is going to make the big money as soon as they step in. Haven’t we seen enough of cash grabs, grifts, and moonboi marketing gimmicks on CT? 
         <br /><br />
         Because quite frankly, it just doesn’t work that way.
         <br /><br />
-        Rather, it all boils down to how well-drilled, prepared, and skilled you are in order to make your name stand out in a space that moves faster than the speed of sound. It’s about constantly learning and upskilling, giving and receiving value to the community of individuals that will end up your most important audience: your stakeholders. Make your own luck and all those things, and web3 will give back.
+        Rather, it all boils down to how well-drilled, prepared, and skilled you are in order to make your name stand out in a space that moves faster than the speed of sound. It’s about constantly learning and upskilling, giving and receiving value to the community of individuals that will end up your most important audience: your stakeholders. Make your own luck through networked squad wealth.
         <br /><br />
         And, in spite of the notions that some individuals might have about the “big money” sloshing around the space, true success is found by those who are relentless.
         <br /><br />
-        In an industry where the market movers and shakers are the same ones culpable for the rampant exploitation, web3’s clarion call is clear–there is hope for a fairer, more sustainable fashion industry. 
+        In an industry where the market movers and shakers are the same ones culpable for the rampant exploitation, web3’s clarion call is clear–there is hope for fairer, more sustainable and economically driven industries. 
 
       </div>
     </div>
