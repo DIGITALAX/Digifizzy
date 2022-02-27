6484d04764d1b90b6f7a2d30abab9f3c69a7aefb
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-02-27 13:06:37 -0500

    add

--- src/components/magazines/issue-10/Page8586.js
+++ src/components/magazines/issue-10/Page8586.js
@@ -52,13 +52,13 @@ const Page8586 = () => (
       I started acting nine years ago! I always loved mimicking what I saw on screen and when I was a teenager, I decided I wanted to go into the profession. Acting was my source of liberation and modeling became another channel of freedom for me! I want to use my talents to help change the world. I wish for others to stand up for what’s right and to do it in style. 
     </div>
     <div className={styles.text3}>
-      There was an unexpected moment when I ran into Bill Clinton. I was on my way back home from eating dinner with a friend. I then saw him about to go into his hotel… of course me being me I had to say hello! I took a pic with him (we were blinded by the flash on my phone lol!).
+      One unexpeted and memoriable moments in my own life journey was when  I ran into Bill Clinton. I was on my way back home from eating dinner with a friend. I then saw him about to go into his hotel… of course me being me I had to say hello! I took a pic with him (we were blinded by the flash on my phone lol!).
     </div>
     <div className={styles.text4}>
-      I will never forget when I got to walk, in my first protest for the BLM movement in June 2020. This experience was so moving for me. My grandfather was a huge advocate for being an active citizen and pushing to get rights for black and brown communities. I’ve always wanted to follow his footsteps and fight for a good cause. 
+      Another unforgettable experience that I will never forget was when I got to walk in my first protest for the BLM movement in June 2020. This experience was so moving for me. My grandfather was a huge advocate for being an active citizen and pushing to get rights for black and brown communities. I’ve always wanted to follow his footsteps and fight for a good cause. 
     </div>
     <div className={styles.text5}>
-      For me it is simple- more spaces for POC, LGBTQ+, and women! It’s expected for creatives to have this mind set, that there should be a safe space, for any and all identities. There needs to be an expansion of open spaces, for all job fields around the globe!
+      In terms of bigger changes that still need to be made on a societal level, well, for me it is simple- more spaces for POC, LGBTQ+, and women! It’s expected for creatives to have this mind set, that there should be a safe space, for any and all identities. There needs to be an expansion of open spaces, for all job fields around the globe!
       <br /><br />
       More inclusion in all spaces and areas. Everyone should be given more access to resources. There should be specific platforms for types of artists such as, one for emerging artists, established artists, and going all the way up to veterans. These platforms would already have a base of people, that want to buy, share, or partake in someones art.
     </div>
