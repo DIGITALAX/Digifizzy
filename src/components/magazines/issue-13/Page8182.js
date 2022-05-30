505997ce50f199d0005d61e93024f67738d7696f
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-05-29 23:41:59 -0400

    video fiz

--- src/components/magazines/issue-13/Page8182.js
+++ src/components/magazines/issue-13/Page8182.js
@@ -54,7 +54,7 @@ const Page8182 = () => (
     </div>
     <iframe
       className={styles.video3}
-      src="https://www.youtube.com/embed/lY893bwmjE4?autoplay=1&mute=1&controls=0&loop=1"
+      src="https://www.youtube.com/watch?v=cbMw7H51ZE0?autoplay=1&mute=1&controls=0&loop=1"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     />
